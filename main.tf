754df52629c6f1c49bab1a03d3e2061c86f64c0a
martingrant01 <martin.nagy01@gmail.com>
2022-01-02 14:54:38 +0100

    feat: add EC2 module to Terraform file for hosting WordPress on it

--- main.tf
+++ main.tf
@@ -17,3 +17,73 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+# Create a VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr_block
+
+  azs             = var.aws_azs
+  public_subnets = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = var.resource_tags
+}
+
+# Create EC2 instance for WordPress
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "wordpress-instance"
+
+  ami                    = "ami-042ad9eec03638628" # Ubuntu Server 18.04 LTS
+  instance_type          = "t2.micro"
+  key_name               = var.ssh_key_WP
+  monitoring             = true
+  vpc_security_group_ids = [aws_security_group.wordpress_instance_SG.id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = var.resource_tags
+}
+
+# Create SG for EC2
+resource "aws_security_group" "wordpress_instance_SG" {
+  name        = "SG for WordPress instance"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow SSH traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = var.resource_tags
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,62 @@ variable "s3_state_bucket" {
     region      = "eu-central-1"
   }
 }
+
+variable "aws_azs" {
+  description = "AZs for the region"
+  type        = list(string)
+  default = [
+    "eu-central-1a",
+    "eu-central-1b",
+    "eu-central-1c"
+  ]
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "test-Martin"
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    owner       = "Martin",
+    environment = "test"
+  }
+}
+
+variable "public_subnet_count" {
+  description = "Number of private subnets."
+  type        = number
+  default     = 2
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets."
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24",
+    "10.0.105.0/24",
+    "10.0.106.0/24",
+    "10.0.107.0/24",
+    "10.0.108.0/24",
+  ]
+}
+
+variable "ssh_key_WP" {
+  description = "SSH key for WordPress EC2"
+  type = string
+  default = "testing-terraform-Martin"	
+}
+
